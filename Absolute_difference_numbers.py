d469e9359f948510bfc1c0a9b7cb5329f5bef77a
21MH1A05A7 <97540659+21MH1A05A7@users.noreply.github.com>
2022-05-02 23:08:15 +0530

    n,m=map(int,input().split()) p=str(n) l=len(p) s=0 q=0 '''for i in range(len(p)):     print(int(p[i]))''' for i in range(0,m):     s=s*10+int(p[i]) for j in range(l-m,l):     q=q*10+int(p[j]) print(abs(q-s))

---
+++ Absolute_difference_numbers.py
@@ -0,0 +1,12 @@
+n,m=map(int,input().split())
+p=str(n)
+l=len(p)
+s=0
+q=0
+'''for i in range(len(p)):
+    print(int(p[i]))'''
+for i in range(0,m):
+    s=s*10+int(p[i])
+for j in range(l-m,l):
+    q=q*10+int(p[j])
+print(abs(q-s))
