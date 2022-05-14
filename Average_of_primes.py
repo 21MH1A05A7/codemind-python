abd19e4f71e3da4f4d03c7e9d8690b83757ccf38
21MH1A05A7 <97540659+21MH1A05A7@users.noreply.github.com>
2022-05-14 19:23:34 +0530

    def prime(n):     for i in range(2,n//2+1):         if(n%i==0):             return False     else:         return True n=int(input()) arr=list(map(int,input().strip().split()))[:n] c=0 s=0 for i in range(n):     if(prime(arr[i]) and arr[i]!=1):         s=s+arr[i]         c+=1 avg=s/c print("{:.2f}".format(avg))

---
+++ Average_of_primes.py
@@ -0,0 +1,17 @@
+def prime(n):
+    for i in range(2,n//2+1):
+        if(n%i==0):
+            return False
+    else:
+        return True
+n=int(input())
+arr=list(map(int,input().strip().split()))[:n]
+c=0
+s=0
+for i in range(n):
+    if(prime(arr[i]) and arr[i]!=1):
+        s=s+arr[i]
+        c+=1
+avg=s/c
+print("{:.2f}".format(avg))
+    
