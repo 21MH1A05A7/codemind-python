723fb57e31d5ea011e1b5bdbc5c1b14d5e758d16
21MH1A05A7 <97540659+21MH1A05A7@users.noreply.github.com>
2022-05-19 19:19:20 +0530

    n=int(input()) arr=list(map(int,input().strip().split()))[:n] c=0 l=[] for i in range(n):     for j in range(n):         if(arr[i]==arr[j] and arr[i] not in l):             c+=1     if(c>=1):         l.append(arr[i])     c=0 for i in range(len(l)):     print(l[i],end=" ")

---
+++ unique_elements.py
@@ -0,0 +1,13 @@
+n=int(input())
+arr=list(map(int,input().strip().split()))[:n]
+c=0
+l=[]
+for i in range(n):
+    for j in range(n):
+        if(arr[i]==arr[j] and arr[i] not in l):
+            c+=1
+    if(c>=1):
+        l.append(arr[i])
+    c=0
+for i in range(len(l)):
+    print(l[i],end=" ")
