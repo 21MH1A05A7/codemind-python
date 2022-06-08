5321be7a87a3c1612557d01b33b9f2b6ccab3965
21MH1A05A7 <97540659+21MH1A05A7@users.noreply.github.com>
2022-06-09 02:46:53 +0530

    n=int(input()) arr=list(map(int,input().split())) q=set(arr) s=0 for i in q:     if(arr.count(i)==1 and i%2==0):         #print(i)         s+=1     elif(arr.count(i)>1 and i%2==0):         s+=1         #print(i)         arr.remove(i) print(s)

---
+++ unique_even_count.py
@@ -0,0 +1,13 @@
+n=int(input())
+arr=list(map(int,input().split()))
+q=set(arr)
+s=0
+for i in q:
+    if(arr.count(i)==1 and i%2==0):
+        #print(i)
+        s+=1
+    elif(arr.count(i)>1 and i%2==0):
+        s+=1
+        #print(i)
+        arr.remove(i)
+print(s)
