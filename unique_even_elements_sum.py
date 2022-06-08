7f19bb82f7dfe6ba67bb494655607cece3fd7b5c
21MH1A05A7 <97540659+21MH1A05A7@users.noreply.github.com>
2022-06-09 02:45:50 +0530

    n=int(input()) arr=list(map(int,input().split())) q=set(arr) s=0 for i in q:     if(arr.count(i)==1 and i%2==0):         #print(i)         s+=i     elif(arr.count(i)>1 and i%2==0):         s+=i         #print(i)         arr.remove(i) print(s)

---
+++ unique_even_elements_sum.py
@@ -0,0 +1,13 @@
+n=int(input())
+arr=list(map(int,input().split()))
+q=set(arr)
+s=0
+for i in q:
+    if(arr.count(i)==1 and i%2==0):
+        #print(i)
+        s+=i
+    elif(arr.count(i)>1 and i%2==0):
+        s+=i
+        #print(i)
+        arr.remove(i)
+print(s)
