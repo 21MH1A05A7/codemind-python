561a6174b24f7b590146040605f982c9863901cd
21MH1A05A7 <97540659+21MH1A05A7@users.noreply.github.com>
2022-06-22 23:00:28 +0530

    n=int(input()) for i in range(n):     a=int(input())     s=str(a)     l=list(s)     l.sort()     #print(l)     c=1     for i in range(len(l)-1):         if(int(l[i+1])-int(l[i])==1):             c+=1     if(c==len(l)):         print("YES")     else:         print("NO")

---
+++ _Help_Jarvis!.py
@@ -0,0 +1,16 @@
+n=int(input())
+for i in range(n):
+    a=int(input())
+    s=str(a)
+    l=list(s)
+    l.sort()
+    #print(l)
+    c=1
+    for i in range(len(l)-1):
+        if(int(l[i+1])-int(l[i])==1):
+            c+=1
+    if(c==len(l)):
+        print("YES")
+    else:
+        print("NO")
+    
