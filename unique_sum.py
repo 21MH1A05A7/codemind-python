b6c1f7316aea8ce10b71ec7066ad66305937313c
21MH1A05A7 <97540659+21MH1A05A7@users.noreply.github.com>
2022-06-28 23:14:31 +0530

    n=int(input()) arr=list(map(int,input().split())) q=set(arr) s=0 for i in q:     if(arr.count(i)==1):         #print(i)         s+=i     elif(arr.count(i)>1):         s+=i         #print(i)         arr.remove(i) print(s)

--- unique_sum.py
+++ unique_sum.py
@@ -1,15 +1,13 @@
 n=int(input())
-arr=list(map(int,input().strip().split()))[:n]
-l=[]
-c=0
-for i in range(n):
-    for j in range(n):
-        if(arr[i]==arr[j] and arr[i] not in l):
-            c+=1
-    if(c>=1):
-        l.append(arr[i])
-    c=0
+arr=list(map(int,input().split()))
+q=set(arr)
 s=0
-for i in range(len(l)):
-    s+=l[i]
+for i in q:
+    if(arr.count(i)==1):
+        #print(i)
+        s+=i
+    elif(arr.count(i)>1):
+        s+=i
+        #print(i)
+        arr.remove(i)
 print(s)
