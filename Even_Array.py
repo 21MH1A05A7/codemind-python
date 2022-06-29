387a2124ff243d8b5103521c34fb09a74fa7d228
21MH1A05A7 <97540659+21MH1A05A7@users.noreply.github.com>
2022-06-29 15:28:53 +0530

    n=int(input()) arr=list(map(int,input().split())) c=0 for i in arr:     if(i%2==0):         c+=1
    if(c==n):
        print(True)
    else:
        print(False)

--- Even_Array.py
+++ Even_Array.py
@@ -1,10 +1,11 @@
 n=int(input())
-arr=list(map(int,input().strip().split()))[:n]
-count=0
-for i in range(len(arr)):
-    if(arr[i]%2==0):
-        count+=1
-if(count==len(arr)):
-    print("True")
+arr=list(map(int,input().split()))
+c=0
+for i in arr:
+    if(i%2==0):
+        c+=1
+        
+if(c==n):
+    print(True)
 else:
-    print("False")
+    print(False)
