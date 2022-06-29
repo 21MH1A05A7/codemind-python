5daf19a3fa2927dbd1114e403738cb6e23f0718f
21MH1A05A7 <97540659+21MH1A05A7@users.noreply.github.com>
2022-06-29 14:12:45 +0530

    n=int(input()) arr=list(map(int,input().split())) avg=sum(arr)//n if(avg in arr):     print(True) else:     print(False)

--- average_element_is_in_array_or_not.py
+++ average_element_is_in_array_or_not.py
@@ -1,15 +1,7 @@
 n=int(input())
-s=0
-a=list(map(int,input().strip().split()))[:n]
-for i in range(n):
-    s=s+a[i]
-av=s//n
-f=0
-for i in range(n):
-    if(av==a[i]):
-        f=1
-        break
-if(f==1):
-    print("True")
+arr=list(map(int,input().split()))
+avg=sum(arr)//n
+if(avg in arr):
+    print(True)
 else:
-    print("False")
+    print(False)
