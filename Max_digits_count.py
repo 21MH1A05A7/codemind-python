53722d0dbb1af7c9cdb91c5e12c9c86e4fee9b17
21MH1A05A7 <97540659+21MH1A05A7@users.noreply.github.com>
2022-06-29 21:43:36 +0530

    def count(n):     q=str(n)     return len(q)
    n=int(input())
    arr=list(map(int,input().split()))
    c=0
    max=0
    for i in arr:
        b=count(i)
        if(b>max):
            max=b
    for i in arr:
        if(len(str(i))==max):
            c+=1
    print(c)

--- Max_digits_count.py
+++ Max_digits_count.py
@@ -1,24 +1,17 @@
+def count(n):
+    q=str(n)
+    return len(q)
+
 n=int(input())
-arr=list(map(int,input().strip().split()))[:n]
-#print(arr)
-l=[]
-d=0
-for i in range(n):
-    p=arr[i]
-    if(p==0):
-        d+=1
-    if(p<0):
-        p=p*-1
-    while(p):
-        d+=1
-        p=p//10
-    l.append(d)
-    d=0
-l.sort()
-#print(l)
-q=0
-max=l[n-1]
-for i in range(n):
-    if(l[i]==max):
-        q+=1
-print(q)
+arr=list(map(int,input().split()))
+c=0
+max=0
+for i in arr:
+    b=count(i)
+    if(b>max):
+        max=b
+for i in arr:
+    if(len(str(i))==max):
+        c+=1
+print(c)
+    
