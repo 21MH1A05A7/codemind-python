c14f0ffc843361a2871185f4bc692a25560b321a
21MH1A05A7 <97540659+21MH1A05A7@users.noreply.github.com>
2022-06-29 14:23:28 +0530

    n=int(input()) arr=list(map(int,input().split())) c=0 count=0 for i in range(n):     if(i%2!=0 and arr[i]%2!=0):         c+=1 for i in arr:     if(i%2!=0):         count+=1 if(count==c):     print(True) else:     print(False)

--- Strictly_ODD.py
+++ Strictly_ODD.py
@@ -1,13 +1,14 @@
 n=int(input())
-arr=list(map(int,input().strip().split()))[:n]
+arr=list(map(int,input().split()))
 c=0
-s=0
+count=0
 for i in range(n):
     if(i%2!=0 and arr[i]%2!=0):
         c+=1
-    if(arr[i]%2!=0):
-        s+=1
-if(c==s):
-    print("True")
+for i in arr:
+    if(i%2!=0):
+        count+=1
+if(count==c):
+    print(True)
 else:
-    print("False")
+    print(False)
