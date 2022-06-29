ebdd6c9c96f82331fe484cc5007ab947e5b04312
21MH1A05A7 <97540659+21MH1A05A7@users.noreply.github.com>
2022-06-29 14:14:28 +0530

    n=int(input()) arr=list(map(int,input().split())) avg=sum(arr)/n print("{:.2f}".format(avg))

--- average_of_the_array.py
+++ average_of_the_array.py
@@ -1,7 +1,4 @@
 n=int(input())
-s=0
-a=list(map(int,input().strip().split()))[:n]
-for i in range(n):
-    s=s+a[i]
-av=s/n
-print("{:.2f}".format(av))
+arr=list(map(int,input().split()))
+avg=sum(arr)/n
+print("{:.2f}".format(avg))
