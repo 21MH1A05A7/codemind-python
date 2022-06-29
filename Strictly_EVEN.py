3dc6529642a7050140cfdef57dcaa7d6c44ad43b
21MH1A05A7 <97540659+21MH1A05A7@users.noreply.github.com>
2022-06-29 15:08:49 +0530

    n=int(input()) arr=list(map(int,input().split())) c=0 count=0 for i in range(n):     if(arr[i]%2==0 and i%2==0):         c+=1 for i in range(n):     if(arr[i]%2==0):         count+=1 if(c==count):     print(True) else:     print(False)

--- Strictly_EVEN.py
+++ Strictly_EVEN.py
@@ -1,13 +1,14 @@
 n=int(input())
-arr=list(map(int,input().strip().split()))[:n]
+arr=list(map(int,input().split()))
 c=0
-s=0
+count=0
 for i in range(n):
-    if(i%2==0 and arr[i]%2==0):
+    if(arr[i]%2==0 and i%2==0):
         c+=1
+for i in range(n):
     if(arr[i]%2==0):
-        s+=1
-if(s==c):
-    print("True")
+        count+=1
+if(c==count):
+    print(True)
 else:
-    print("False")
+    print(False)
