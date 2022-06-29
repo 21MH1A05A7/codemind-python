8242d23cde646bef0d85e58f2e60e5aa5f0a50b2
21MH1A05A7 <97540659+21MH1A05A7@users.noreply.github.com>
2022-06-29 21:53:48 +0530

    def palin(n):     q=str(n)     return q[::-1] n=int(input()) arr=list(map(int,input().split())) l=[] for i in arr:     b=palin(i)     l.append(int(b)) print(*l)

--- Reverse_game.py
+++ Reverse_game.py
@@ -1,9 +1,10 @@
+def palin(n):
+    q=str(n)
+    return q[::-1]
 n=int(input())
-arr=list(map(int,input().strip().split()))[:n]
+arr=list(map(int,input().split()))
 l=[]
-for i in range(n):
-    q=str(arr[i])
-    r=q[::-1]
-    l.append(int(r))
-for i in range(n):
-    print(l[i],end=" ")
+for i in arr:
+    b=palin(i)
+    l.append(int(b))
+print(*l)
