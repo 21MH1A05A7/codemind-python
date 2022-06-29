e03f8babda1b440c9faced1a5e4f2b920778a6da
21MH1A05A7 <97540659+21MH1A05A7@users.noreply.github.com>
2022-06-29 14:18:39 +0530

    n=int(input()) arr=list(map(int,input().split())) print(min(arr))

--- minimum_element_of_an_array.py
+++ minimum_element_of_an_array.py
@@ -1,8 +1,3 @@
 n=int(input())
-arr=list(map(int,input().strip().split()))[:n]
-m=arr[0]
-for i in range(1,len(arr)):
-    if(arr[i]<m):
-        m=arr[i]
-print(m)
-    
+arr=list(map(int,input().split()))
+print(min(arr))
