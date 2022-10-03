3671af59dc4d4bf091c4839005b6b97b2fa60965
21MH1A05A7 <97540659+21MH1A05A7@users.noreply.github.com>
2022-10-03 19:11:46 +0530

    def prime(n):     if(n==1):         return False     else:         for i in range(2,n//2+1):             if(n%i==0):                 return False         else:             return True n=int(input()) if(prime(n)):     print("prime") else:     print("not a prime")

--- prime_number.py
+++ prime_number.py
@@ -1,11 +1,14 @@
+def prime(n):
+    if(n==1):
+        return False
+    else:
+        for i in range(2,n//2+1):
+            if(n%i==0):
+                return False
+        else:
+            return True
 n=int(input())
-f=0
-if(n!=1):
-    for i in range(2,n):
-        if(n%i==0):
-            f=1
-            break
-if(f==1):
-    print("not a prime")
-else:
+if(prime(n)):
     print("prime")
+else:
+    print("not a prime")
