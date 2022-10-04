68dde458b8218032f8f91fe589ff2d2cc48b2d8f
21MH1A05A7 <97540659+21MH1A05A7@users.noreply.github.com>
2022-10-04 11:35:40 +0530

    def prime(n):     if(n==1):         return False     else:         for i in range(2,n//2+1):             if(n%i==0):                 return False         else:             return True n=int(input()) arr=list(map(int,input().split())) #print(arr) k=int(input()) #print(k) c=0 for i in range(n):     if(arr[i]>=k):         if(prime(arr[i])):             c+=1 print(c)

--- Primes_greater_than_k.py
+++ Primes_greater_than_k.py
@@ -1,14 +1,20 @@
-
+def prime(n):
+    if(n==1):
+        return False
+    else:
+        for i in range(2,n//2+1):
+            if(n%i==0):
+                return False
+        else:
+            return True
 n=int(input())
-arr=list(map(int,input().strip().split()))[:n]
-a=int(input())
+arr=list(map(int,input().split()))
+#print(arr)
+k=int(input())
+#print(k)
 c=0
 for i in range(n):
-    p=arr[i]
-    if(arr[i]>=a and arr[i]!=1):
-        for j in range(2,p//2+1):
-            if(p%j==0):
-                break
-        else:
+    if(arr[i]>=k):
+        if(prime(arr[i])):
             c+=1
 print(c)
