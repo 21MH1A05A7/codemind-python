e3cdccb881572fb516705185b2a761c3cdc9b6ef
21MH1A05A7 <97540659+21MH1A05A7@users.noreply.github.com>
2022-10-10 23:16:53 +0530

    t=int(input()) for i in range(t):     n=int(input())     arr=list(map(int,input().split()))     c=0     for i in range(1,max(arr)+1):         if(i not in arr):             print(i)             c=1             break     if(c==0):         print(max(arr)+1)

---
+++ Missing_number_in_array.py
@@ -0,0 +1,12 @@
+t=int(input())
+for i in range(t):
+    n=int(input())
+    arr=list(map(int,input().split()))
+    c=0
+    for i in range(1,max(arr)+1):
+        if(i not in arr):
+            print(i)
+            c=1
+            break
+    if(c==0):
+        print(max(arr)+1)
