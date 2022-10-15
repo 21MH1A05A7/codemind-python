4f88c67e9b88db8e777b7bcc080e9f7428c4e1ef
21MH1A05A7 <97540659+21MH1A05A7@users.noreply.github.com>
2022-10-15 22:26:32 +0530

    n=input() s=n.split() q=s[::-1] for i in q:     k=i[::-1]     print(k,end=' ')

--- reverse_words_and_positions.py
+++ reverse_words_and_positions.py
@@ -1,10 +1,7 @@
 n=input()
-q=n.split()
-for i in range(len(q)):
-    p=q[i]
-    r=p[::-1]
-    q[i]=r
-#print(q)
-a=len(q)
-for i in range(a-1,-1,-1):
-    print(q[i],end=" ")
+s=n.split()
+q=s[::-1]
+for i in q:
+    k=i[::-1]
+    print(k,end=' ')
+    
