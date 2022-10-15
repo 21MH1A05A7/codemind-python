93e17e1ed3649f4eeae5ffb0bae52e82d121f5a0
21MH1A05A7 <97540659+21MH1A05A7@users.noreply.github.com>
2022-10-15 22:33:42 +0530

    n=input() q=n.split() l=['a','e','i','o','u'] max=0 c=0 for i in range(len(q)):     for j in range(len(q[i])):         if(q[i][j] in l):             c+=1     if(c>max):         max=c     c=0 print(max)

--- maximum_vowel_count.py
+++ maximum_vowel_count.py
@@ -3,7 +3,7 @@ q=n.split()
 l=['a','e','i','o','u']
 max=0
 c=0
-for i in range(len(q)): #"technical"  "hub"
+for i in range(len(q)):
     for j in range(len(q[i])):
         if(q[i][j] in l):
             c+=1
