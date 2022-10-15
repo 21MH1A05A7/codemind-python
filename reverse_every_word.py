c7b93051ff7e4c7fe05993fb4e12b6914ea42c6e
21MH1A05A7 <97540659+21MH1A05A7@users.noreply.github.com>
2022-10-15 22:22:07 +0530

    n=input() s=n.split() for i in s:     print(i[::-1],end=' ')

--- reverse_every_word.py
+++ reverse_every_word.py
@@ -1,8 +1,4 @@
 n=input()
-q=n.split()
-for i in range(len(q)):
-    p=q[i]
-    r=p[::-1]
-    q[i]=r
-for i in range(len(q)):
-    print(q[i],end=" ")
+s=n.split()
+for i in s:
+    print(i[::-1],end=' ')
