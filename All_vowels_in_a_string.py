e307986b8643c345b3025b91f44d10f6d36746b6
21MH1A05A7 <97540659+21MH1A05A7@users.noreply.github.com>
2022-10-16 10:41:50 +0530

    n=input() k=n.replace(" ","") l=["a","e","i","o","u"] s=set(k) for i in s:     if(i in l):         l.remove(i) if(len(l)==0):     print("True") else:     print("False")

--- All_vowels_in_a_string.py
+++ All_vowels_in_a_string.py
@@ -1,18 +1,11 @@
 n=input()
-l=['a','e','i','o','u']
-a=['A','E','I','O','U']
-k=[]
-q=[]
-c=0
-s=0
-for i in n:
-    if(i in l and i not in k):
-        c+=1
-        k.append(i)
-    elif(i in a and i not in q):
-        s+=1
-        q.append(i)
-if(c==len(l) or s==len(a)):
-    print(True)
+k=n.replace(" ","")
+l=["a","e","i","o","u"]
+s=set(k)
+for i in s:
+    if(i in l):
+        l.remove(i)
+if(len(l)==0):
+    print("True")
 else:
-    print(False)
+    print("False")
