526feea0fe0ec2e469cb7bfabddfab6bfa8933cc
21MH1A05A7 <97540659+21MH1A05A7@users.noreply.github.com>
2022-10-16 11:21:50 +0530

    n=input() re_sp=n.replace(" ","") lw=re_sp.lower() s=set(lw)
    if(len(s)==26):
        print("True")
    else:
        print("False")

--- Pangram_or_not.py
+++ Pangram_or_not.py
@@ -1,14 +1,9 @@
 n=input()
-n1=n.lower()
-l=list(n1)
-space_count=l.count(" ")
-for i in range(space_count):
-    l.remove(" ")
-for i in l:
-    if(i.isupper()):
-        i.lower()
-q=set(l)
-if(len(q)==26):
-    print(True)
+re_sp=n.replace(" ","")
+lw=re_sp.lower()
+s=set(lw)
+
+if(len(s)==26):
+    print("True")
 else:
-    print(False)
+    print("False")
